f5f3bc7dcd73085a01cf6a3716cab2e942c1107c
Tovah Parnes <tovah@parnes.com>
2021-02-15 09:54:59 +0100

    finished part 3

---
+++ Lab-3/Alternative-3/Alternative-3/tinythreads.c
@@ -0,0 +1,137 @@
+#include <setjmp.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdbool.h>
+#include "tinythreads.h"
+
+#define NULL            0
+#define DISABLE()       cli()
+#define ENABLE()        sei()
+#define STACKSIZE       80
+#define NTHREADS        4
+#define SETSTACK(buf,a) *((unsigned int *)(buf)+8) = (unsigned int)(a) + STACKSIZE - 4; \
+                        *((unsigned int *)(buf)+9) = (unsigned int)(a) + STACKSIZE - 4
+						
+
+struct thread_block {
+    void (*function)(int);   // code to run
+    int arg;                 // argument to the above
+    thread next;             // for use in linked lists
+    jmp_buf context;         // machine state
+    char stack[STACKSIZE];   // execution stack space
+};
+
+struct thread_block threads[NTHREADS];
+
+struct thread_block initp;
+
+thread freeQ   = threads;
+thread readyQ  = NULL;
+thread current = &initp;
+
+int initialized = 0;
+
+static void initialize(void) {
+    int i;
+    for (i=0; i<NTHREADS-1; i++){
+        threads[i].next = &threads[i+1];
+	}
+    threads[NTHREADS-1].next = NULL;
+
+
+    initialized = 1;
+	
+	// Initialize for butterfly joystick
+	EIMSK = (1<<PCIE1) | EIMSK;
+	PCMSK1 = (1<<PCINT15) | PCMSK1;
+	PORTB = (1<<PB7) | PORTB;
+	
+	// Initialize for timer 
+	
+	// Enable timer output compare A interrupts 
+	TIMSK1 = (1<<OCIE1A) | TIMSK1;
+	// Turn on OC1A compare match
+	TCCR1A = (1<<COM1A0) | (1<<COM1A1) | TCCR1A;
+	// Set timer to clear on timer compare (CTC) and timer prescaling factor 1024 
+	TCCR1B = (1<<WGM12) | (1<<CS12) | (1<<CS10) | TCCR1B;
+	
+	// 20 intervals of 50 ms per second 
+	// 1 second = 8 000 000 / 1024 = 7812.5 
+	// 1 second divided by 2 intervals = 7812.5/2 = 3906,25 = 0xF42
+	OCR1A = 0xF42;	
+	TCNT1 = 0x0;
+	
+}
+
+static void enqueue(thread p, thread *queue) {
+    p->next = NULL;
+    if (*queue == NULL) {
+        *queue = p;
+    } else {
+        thread q = *queue;
+		p->next = q;
+        *queue = p;
+    }
+}
+
+static thread dequeue(thread *queue) {
+    thread p = *queue;
+    if (*queue) {
+        *queue = (*queue)->next;
+    } else {
+        // Empty queue, kernel panic!!!
+        while (1) ;  // not much else to do...
+    }
+    return p;
+}
+
+static void dispatch(thread next) {
+    if (setjmp(current->context) == 0) {
+        current = next;
+        longjmp(next->context,1);
+    }
+}
+
+void spawn(void (* function)(int), int arg) {
+    thread newp;
+
+    DISABLE();
+    if (!initialized) initialize();
+	enqueue(current, &readyQ);
+
+    newp = dequeue(&freeQ);
+    newp->function = function;
+    newp->arg = arg;
+    newp->next = NULL;
+    if (setjmp(newp->context) == 1) {
+        ENABLE();
+        current->function(current->arg);
+        DISABLE();
+        enqueue(current, &freeQ);
+        dispatch(dequeue(&readyQ));
+    }
+    SETSTACK(&newp->context, &newp->stack);
+
+    enqueue(newp, &readyQ);
+	ENABLE();
+	dispatch(dequeue(&readyQ));
+}
+
+ISR(PCINT1_vect){
+	//Only spawn on the ricing edge
+	if (( PINB & 1<<PB7) == 0){
+		DISABLE();
+		
+		spawn(button, 3);
+		
+		ENABLE();
+	}
+}
+
+ISR(TIMER1_COMPA_vect){
+	DISABLE();
+	//enqueue(current, &readyQ);
+	spawn(blink, 20);
+	//dispatch(dequeue(&readyQ));
+	ENABLE();
+}
